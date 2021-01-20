d9ef2b76e45bb895a6f3ec42a3a696e628948ea5
luislasn <luis.lasn@gmail.com>
2021-01-20 18:10:18 -0300

    Criação do método que busca as ordens de serviço pesquisando pelo id

--- src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
@@ -1,6 +1,7 @@
 package com.algaworks.osworks.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -8,7 +9,9 @@ import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,6 +48,19 @@ public class OrdemServicoController {
 	@GetMapping
 	public List<OrdemServicoModel> listar() {
 		return toCollectionModel(ordemServicoRepository.findAll());
+	}
+	
+	@GetMapping("/{ordemServicoId}")
+	public ResponseEntity<OrdemServicoModel> buscar(@PathVariable Long ordemServicoId) {
+		
+		Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
+		
+		if (ordemServico.isPresent()) {
+			OrdemServicoModel ordemServicoModel = toModel(ordemServico.get());
+			return ResponseEntity.ok(ordemServicoModel);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 
 	private List<OrdemServicoModel> toCollectionModel(List<OrdemServico> ordensServico) {
