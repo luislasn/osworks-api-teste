c4181d814a14708f03f1ed5a989efe227c1070f8
luislasn <luis.lasn@gmail.com>
2021-01-20 16:16:20 -0300

    Commit inicial do projeto osworks-api-teste

---
+++ src/main/java/com/algaworks/osworks/OsworksApiTesteApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.osworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsworksApiTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsworksApiTesteApplication.class, args);
+	}
+
+}
